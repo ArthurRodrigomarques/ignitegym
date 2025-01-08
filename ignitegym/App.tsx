a21fd167cbeae629643e0101e89b623bbc1edcde
ArthurRodrigomarques <arthurrodrigo05052004@gmail.com>
2025-01-08 07:12:22 -0300

    add
    add

---
+++ ignitegym/App.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center',}}>
+      {fontsLoaded ? <Text>Home</Text> : <View/>}
+    </View>
+  );
+}
+
+
