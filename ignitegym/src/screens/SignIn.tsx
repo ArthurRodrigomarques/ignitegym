e46c8c5c7d1515df0d2019604cd7de249a023551
ArthurRodrigomarques <arthurrodrigo05052004@gmail.com>
2025-01-09 13:18:31 -0300

    button
    button

--- ignitegym/src/screens/SignIn.tsx
+++ ignitegym/src/screens/SignIn.tsx
@@ -4,6 +4,7 @@ import BackgroundImg from "@assets/background.png"
 import Logo from "@assets/logo.svg"
 
 import { Input } from "@components/Input"
+import { Button } from "@components/Button";
 
 export function SignIn() {
     return (
@@ -32,6 +33,8 @@ export function SignIn() {
                     autoCapitalize="none"
                     />
                     <Input placeholder="Senha" secureTextEntry/>
+
+                    <Button title="Acessar" isLoading/>
                 </Center>
 
             </Center>
