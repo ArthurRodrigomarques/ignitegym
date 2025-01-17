05f88a5da66f4de2212c2de47370789db08adbfa
ArthurRodrigomarques <arthurrodrigo05052004@gmail.com>
2025-01-17 17:14:00 -0300

    add
    add

--- ignitegym/src/components/Input.tsx
+++ ignitegym/src/components/Input.tsx
@@ -1,31 +1,54 @@
-import { Input as GlueStackInput, InputField } from "@gluestack-ui/themed";
 import { ComponentProps } from "react";
 
+import {
+  Input as GlueStackInput,
+  InputField,
+  FormControl,
+  FormControlError,
+  FormControlErrorText,
+} from "@gluestack-ui/themed";
+
 type Props = ComponentProps<typeof InputField> & {
+  errorMessage?: string | null;
+  isInvalid?: boolean;
   isReadOnly?: boolean;
 };
 
-export function Input({ isReadOnly = false, ...rest }: Props) {
+export function Input({
+  isReadOnly = false,
+  errorMessage = null,
+  isInvalid = false,
+  ...rest
+}: Props) {
+  const invalid = !!errorMessage || isInvalid;
+
   return (
-    <GlueStackInput
-      h="$14"
-      borderWidth="$0"
-      borderRadius="$md"
-      $focus={{
-        borderWidth: 1,
-        borderColor: "$green500",
-      }}
-      isReadOnly={isReadOnly}
-      opacity={isReadOnly ? 0.5 : 1}
-    >
-      <InputField
-        px="$4"
-        bg="$trueGray700"
-        color="$white"
-        fontFamily="$body"
-        placeholderTextColor="$trueGray300"
-        {...rest}
-      />
-    </GlueStackInput>
+    <FormControl isInvalid={invalid} w="$full" mb="$4">
+      <GlueStackInput
+        h="$14"
+        borderWidth="$0"
+        borderRadius="$md"
+        $focus={{
+          borderWidth: 1,
+          borderColor: "$green500",
+        }}
+        isReadOnly={isReadOnly}
+        opacity={isReadOnly ? 0.5 : 1}
+      >
+        <InputField
+          px="$4"
+          bg="$trueGray700"
+          color="$white"
+          fontFamily="$body"
+          placeholderTextColor="$trueGray300"
+          {...rest}
+        />
+      </GlueStackInput>
+      <FormControlError>
+        <FormControlErrorText color="$red500">
+          {errorMessage}
+        </FormControlErrorText>
+      </FormControlError>
+    </FormControl>
   );
 }
--- ignitegym/src/screens/SignUp.tsx
+++ ignitegym/src/screens/SignUp.tsx
@@ -6,6 +6,7 @@ import {
   Heading,
   ScrollView,
 } from "@gluestack-ui/themed";
+import { useForm, Controller } from "react-hook-form";
 
 import BackgroundImg from "@assets/background.png";
 import Logo from "@assets/logo.svg";
@@ -15,11 +16,33 @@ import { Button } from "@components/Button";
 
 import { useNavigation } from "@react-navigation/native";
 
+type FormDataProps = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirm: string;
+};
+
 export function SignUp() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataProps>();
+
   const navigation = useNavigation();
 
   function handleGoBack() {
     navigation.goBack();
+  }
+
+  function handleSignUp({
+    name,
+    email,
+    password,
+    password_confirm,
+  }: FormDataProps) {
+    console.log({ name, email, password, password_confirm });
   }
 
   return (
@@ -50,16 +73,76 @@ export function SignUp() {
                 Crie sua conta
               </Heading>
 
-              <Input placeholder="Nome" />
-
-              <Input
-                placeholder="E-mail"
-                keyboardType="email-address"
-                autoCapitalize="none"
+              <Controller
+                control={control}
+                name="name"
+                rules={{
+                  required: "Informe o nome.",
+                }}
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    placeholder="Nome"
+                    onChangeText={onChange}
+                    value={value}
+                    errorMessage={errors.name?.message}
+                  />
+                )}
               />
-              <Input placeholder="Senha" secureTextEntry />
 
-              <Button title="Criar e acessar" />
+              <Controller
+                control={control}
+                name="email"
+                rules={{
+                  required: "Informe o e-mail",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: "Email invalido",
+                  },
+                }}
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    placeholder="E-mail"
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    onChangeText={onChange}
+                    value={value}
+                    errorMessage={errors.email?.message}
+                  />
+                )}
+              />
+
+              <Controller
+                control={control}
+                name="password"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    placeholder="Senha"
+                    secureTextEntry
+                    onChangeText={onChange}
+                    value={value}
+                  />
+                )}
+              />
+
+              <Controller
+                control={control}
+                name="password_confirm"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    placeholder="Confirme a Senha"
+                    secureTextEntry
+                    onChangeText={onChange}
+                    value={value}
+                    onSubmitEditing={handleSubmit(handleSignUp)}
+                    returnKeyType="send"
+                  />
+                )}
+              />
+
+              <Button
+                title="Criar e acessar"
+                onPress={handleSubmit(handleSignUp)}
+              />
             </Center>
 
             <Button
